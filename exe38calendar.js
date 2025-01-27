8e84f6e6254f815c0dea9e2e82ea0882dce92369
greabandrei <greabandrei1@gmail.com>
2025-01-27 16:46:13 +0200

    Update and rename exerctii.js to exe38calendar.js

--- exe38calendar.js
+++ exe38calendar.js
@@ -51,21 +51,3 @@ for(let i = 0; i < 7; i++) {
 }
 console.log(row)
 
-/*
-Exercise 39: String Multiplication
-Implement multiplication of two large numbers represented as strings.
-Don't convert to numbers (numbers might be too large).
-Example:
-Input: "123", "456"
-Output: "56088"
-*/
-function multiplyStrings(num1, num2) {
-    // implementation here
-    let num3;
-    num3 = (num1 * num2);
-    return num3;
-}
-console.log(multiplyStrings("123", "456"));     // "56088"
-console.log(multiplyStrings("999", "999"));     // "998001"
-console.log(multiplyStrings("0", "52"));        // "0"
-//nu vad o modalitate mai simpla. Care e defapt perspectiva?
